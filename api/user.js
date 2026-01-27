238ef04cf7058ad21af02be329d38ddee65236f0
dexblox <ariefcorp03@gmail.com>
2026-01-27 12:15:28 +0700

    add user.js endpoint
    Create an API handler to fetch user data by username.

---
+++ api/user.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  const { username } = req.query;
+  try {
+    const response = await fetch("https://users.roblox.com/v1/usernames/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ usernames: [username] })
+    });
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
