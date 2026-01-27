7b7fdf5158960a17c0dd0339a8e8e26ce619b263
dexblox <ariefcorp03@gmail.com>
2026-01-27 12:20:45 +0700

    add avatar.js endpoint

---
+++ api/avatar.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  const { userId } = req.query;
+  try {
+    const response = await fetch(
+      `https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=${userId}&size=150x150&format=Png&isCircular=false`
+    );
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
